7ba697faa4ce0a5a5df3abe0d9ccd7ae7410b43f
mengxiyume <94777252+mengxiyume@users.noreply.github.com>
2022-09-22 19:43:20 +0800

    202209220743pm

--- Test/KeyBoardAsyncState.cpp
+++ Test/KeyBoardAsyncState.cpp
@@ -1,24 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include"../Include/keyBoard.h"
+#include"./KeyBordAsyncState.h"
 #include<Windows.h>
 #include<stdio.h>
 
-__declspec(dllexport) void keyBoardAsyncKeyState(KeyBoard* key_board)
+#define MAX_KEY_VALUE 0xFF
+#define DEBUG
+
+#undef DEBUG
+
+void keyBoardAsyncKeyState(KeyBoard* key_board, KeyBoard* open_key)
 {
 	int i = 0;
 	for (i = 0; i <= MAX_KEY_VALUE; i++)
 	{
-		if (GetAsyncKeyState(i))
+		if (((bool*)open_key)[i])
 		{
+			if (GetAsyncKeyState(i))
+			{
 #ifdef DEBUG
-			printf("%02X\n", i);
+				printf("%02X\n", i);
 #endif
-			((bool*)key_board)[i] = true;
-		}
-		else
-		{
-			((bool*)key_board)[i] = false;
+				((bool*)key_board)[i] = true;
+			}
+			else
+			{
+				((bool*)key_board)[i] = false;
+			}
 		}
 	}
 }
